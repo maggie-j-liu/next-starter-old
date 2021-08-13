99388d022238764321196c69f520a7292b54e7fb
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-13 14:48:38 -0700

    add firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UserContextProvider } from "@/utils/useUser";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserContextProvider>
+      <Component {...pageProps} />
+    </UserContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
